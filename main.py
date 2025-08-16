8c3ebc96b79f7e66fbd0168b389f2c825b87e346
albertostizzoli <albertostizzoli@yahoo.it>
2025-08-16 14:43:13 +0200

    create posts and create function for to obtain all posts

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
-from fastapi import FastAPI
+from fastapi import FastAPI # importo FastAPI
 
-app = FastAPI()
+app = FastAPI() # creo l'istanza di FastAPI
 
-@app.get("/")
-def home():
-    return {"messaggio": "Ciao da FastAPI!"}
+# Posts
+posts = [
+    {"id": 1, "title": "Vacanze", "content": "Che bello andare in vacanza!"},
+    {"id": 2, "title": "Informatica", "content": "L'informatica è interessante!"},
+    {"id": 3, "title": "Videogiochi", "content": "I videogiochi sono divertenti!"},
+]
+
+# creo una funzione per ottenere tutti i post
+@app.get("/posts")
+def get_posts():
+    return posts
